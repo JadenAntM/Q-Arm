bf3175cf7154895277dfb462c7ee537e5c4d4d9b
JadenAntM <144370058+JadenAntM@users.noreply.github.com>
2023-10-14 22:34:05 -0400

    Update QuanserCode.py

--- QuanserCode.py
+++ QuanserCode.py
@@ -1,5 +1,4 @@
 ip_address = 'localhost' # Enter your IP Address here
-project_identifier = 'P2B' # Enter the project identifier i.e. P2A or P2B
 #--------------------------------------------------------------------------------
 import sys
 sys.path.append('../')
@@ -9,8 +8,6 @@ hardware = False
 QLabs = configure_environment(project_identifier, ip_address, hardware).QLabs
 arm = qarm(project_identifier,ip_address,QLabs,hardware)
 potentiometer = potentiometer_interface()
-#--------------------------------------------------------------------------------
-# STUDENT CODE BEGINS
 #---------------------------------------------------------------------------------
 import random
 
